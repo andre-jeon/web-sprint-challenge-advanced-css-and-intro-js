d689ad9e8d455e1dae86306ad5b75f42d377fa2d
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-06-12 12:52:48 -0400

    Task 4 complete

--- index.js
+++ index.js
@@ -208,11 +208,14 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+artists[8].name = "Vincent van Gogh"
+console.log(artists[8].name);
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -223,20 +226,28 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+    artistByIndex = array[index];
+    return `The artist at index ${artistByIndex.id} is ${artistByIndex.name}.`
   }
   
-  /**
+console.log(getArtistByIndex(artists, 0));
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who lived in the 20th century (1900-2000) */
 
-function get20s(/* Code here */){
+function get20s(arr){
 
-  /* Code here */
+let artist20 = [];
 
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].years === "1907 - 1954" || arr[i].years === "1904 - 1989"){
+      artist20.push(arr[i])
+    }
+  }
+  return artist20;
 }
 
+console.log(get20s(artists));
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
  *     (1) artists array
